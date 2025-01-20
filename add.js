9e41b4dda95134f0302a812028b5599dce951244
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-01-20 22:19:23 +0530

    Fix negative numbers

--- add.js
+++ add.js
@@ -7,9 +7,14 @@ function add(input){
         const cleanedInput = input.split('\n')[1]; 
         const numbers = cleanedInput.split(delimiter).map(Number);
         return numbers.reduce((sum, num) => sum + num, 0);
-    }
+    }    
 
     const numbers = input.split(delimiter).map(Number)
+    const negatives = numbers.filter((num) => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed ${negatives.join(', ')}`);
+    }
+    
     return numbers.reduce((sum,num)=>sum+num,0)
 
 }
