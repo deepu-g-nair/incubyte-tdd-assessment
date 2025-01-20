96806efa8e9f6beaeb4082b26eab69c2bbbc4cf3
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-01-20 21:39:07 +0530

    Fix newline between numbers

--- add.js
+++ add.js
@@ -2,8 +2,12 @@ function add(input){
     if(input == '') return 0
     if(!input.includes(',')) return Number(input)
     
-    const numbers = input.split(',').map(Number)
+    let delimiter =  /[\n,]/
+
+    const numbers = input.split(delimiter).map(Number)
     return numbers.reduce((sum,num)=>sum+num,0)
+
+
 }
 
 module.exports = add
