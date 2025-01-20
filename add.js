1e9983ac5d2f0cc4606d511e4fbdb0f1cedcc562
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-01-20 22:16:36 +0530

    Fix handling custom delimiters

--- add.js
+++ add.js
@@ -1,12 +1,16 @@
 function add(input){
     if(input == '') return 0
-    if(!input.includes(',')) return Number(input)
     
     let delimiter =  /[\n,]/
+    if (input.startsWith('//')) {
+        delimiter = new RegExp(`\\${input[2]}`);
+        const cleanedInput = input.split('\n')[1]; 
+        const numbers = cleanedInput.split(delimiter).map(Number);
+        return numbers.reduce((sum, num) => sum + num, 0);
+    }
 
     const numbers = input.split(delimiter).map(Number)
     return numbers.reduce((sum,num)=>sum+num,0)
-
 
 }
 
