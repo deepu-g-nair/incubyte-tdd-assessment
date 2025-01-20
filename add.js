88a2f3cc9009ddc5c836e4d9c205a6ca468a4b4d
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-01-20 21:24:24 +0530

    Fix test case for handling comma seperated numbers

--- add.js
+++ add.js
@@ -1,6 +1,9 @@
 function add(input){
     if(input == '') return 0
     if(!input.includes(',')) return Number(input)
+    
+    const numbers = input.split(',').map(Number)
+    return numbers.reduce((sum,num)=>sum+num,0)
 }
 
 module.exports = add
