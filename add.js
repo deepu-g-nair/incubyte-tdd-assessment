7db17c51b78bf396b48c275f42e194293e8f63f8
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-01-20 22:22:40 +0530

    Format the code

--- add.js
+++ add.js
@@ -1,22 +1,21 @@
-function add(input){
-    if(input == '') return 0
-    
-    let delimiter =  /[\n,]/
-    if (input.startsWith('//')) {
+function add(input) {
+    if (input == "") return 0;
+
+    let delimiter = /[\n,]/;
+    if (input.startsWith("//")) {
         delimiter = new RegExp(`\\${input[2]}`);
-        const cleanedInput = input.split('\n')[1]; 
+        const cleanedInput = input.split("\n")[1];
         const numbers = cleanedInput.split(delimiter).map(Number);
         return numbers.reduce((sum, num) => sum + num, 0);
-    }    
+    }
 
-    const numbers = input.split(delimiter).map(Number)
+    const numbers = input.split(delimiter).map(Number);
     const negatives = numbers.filter((num) => num < 0);
     if (negatives.length > 0) {
-        throw new Error(`negative numbers not allowed ${negatives.join(', ')}`);
+        throw new Error(`negative numbers not allowed ${negatives.join(", ")}`);
     }
-    
-    return numbers.reduce((sum,num)=>sum+num,0)
 
+    return numbers.reduce((sum, num) => sum + num, 0);
 }
 
-module.exports = add
+module.exports = add;
--- add.test.js
+++ add.test.js
@@ -1,32 +1,32 @@
-const add = require('./add')
+const add = require("./add");
 
-test('returns 0 if the input is an empty string', ()=>{
-    expect(add('')).toBe(0)
-})
+test("returns 0 if the input is an empty string", () => {
+    expect(add("")).toBe(0);
+});
 
-test('returns the number itslef if the input is a single number',()=>{
-    expect(add('1')).toBe(1)
-})
+test("returns the number itslef if the input is a single number", () => {
+    expect(add("1")).toBe(1);
+});
 
-test('returns the sum if the input is comma seperated numbers', ()=>{
-    expect(add('1,5')).toBe(6)
-})
+test("returns the sum if the input is comma seperated numbers", () => {
+    expect(add("1,5")).toBe(6);
+});
 
-test('handle any amount of numbers',()=>{
-    expect(add('1,5,6,1,5,6')).toBe(24)
-})
+test("handle any amount of numbers", () => {
+    expect(add("1,5,6,1,5,6")).toBe(24);
+});
 
-test('handle new lines between numbers instead of commas',()=>{
-    expect(add('1\n2,3')).toBe(6)
-})
+test("handle new lines between numbers instead of commas", () => {
+    expect(add("1\n2,3")).toBe(6);
+});
 
-test('handle custom delimiters', () => {
-    expect(add('//;\n1;2')).toBe(3);
-    expect(add('//|\n1|2|3')).toBe(6);
-})
+test("handle custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//|\n1|2|3")).toBe(6);
+});
 
-test('throws an exception for negative numbers', () => {
-    expect(() => add('-1')).toThrow('negative numbers not allowed -1');
-    expect(() => add('1,-2,3')).toThrow('negative numbers not allowed -2');
-    expect(() => add('-1,-2,3')).toThrow('negative numbers not allowed -1, -2');
-})
+test("throws an exception for negative numbers", () => {
+    expect(() => add("-1")).toThrow("negative numbers not allowed -1");
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed -2");
+    expect(() => add("-1,-2,3")).toThrow("negative numbers not allowed -1, -2");
+});
