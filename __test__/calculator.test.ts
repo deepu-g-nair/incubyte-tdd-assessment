9901aba6615774d9a4c92c32058ce31cd50ad2fa
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-02-01 19:45:01 +0530

    negative numbers case fail

--- __test__/calculator.test.ts
+++ __test__/calculator.test.ts
@@ -24,3 +24,9 @@ test("handle custom delimiters", () => {
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//|\n1|2|3")).toBe(6);
 });
+
+test("throws an exception for negative numbers", () => {
+    expect(() => add("-1")).toThrow("negative numbers not allowed -1");
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed -2");
+    expect(() => add("-1,-2,3")).toThrow("negative numbers not allowed -1, -2");
+});
