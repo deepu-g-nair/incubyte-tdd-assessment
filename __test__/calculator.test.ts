3edc8f0b31a81d2227c5c9f4fc1b0451f8fd345e
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-02-01 19:22:18 +0530

    single number input case

--- __test__/calculator.test.ts
+++ __test__/calculator.test.ts
@@ -3,3 +3,7 @@ import { add } from '../utils/calculator';
 test("returns 0 if the input is an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("returns the number itslef if the input is a single number", () => {
+    expect(add("1")).toBe(1);
+});
