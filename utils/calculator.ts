e8913b75c2acf785c369f09047f19bc253bdc1fc
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-02-01 19:42:24 +0530

    handle custom delimiter case pass

--- utils/calculator.ts
+++ utils/calculator.ts
@@ -1,15 +1,25 @@
 function add(input: string): number {
-    if(input == ''){
-        return 0
-    }
-    else{
-        const numbers = input.split(/[\n,]+/).map(Number);
-        if(numbers.length == 1){
-            return numbers[0]
+    if (input == "") {
+        return 0;
+    } else {
+        if (input.startsWith("//")) {
+            const delimiter = input.charAt(2);
+            const numbers = input.slice(4).split(delimiter).map(Number);
+
+            if (numbers.length == 1) {
+                return numbers[0];
+            } else {
+                const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+                return sum;
+            }
         }
-        else{
+
+        const numbers = input.split(/[\n,]+/).map(Number);
+        if (numbers.length == 1) {
+            return numbers[0];
+        } else {
             const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-            return sum
+            return sum;
         }
     }
 }
