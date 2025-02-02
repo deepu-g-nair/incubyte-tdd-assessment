b0024db48f86621443c9a33d478d521a2467d92a
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-02-02 10:57:46 +0530

    ui updates display when numbers pressed fail

--- __test__/CalculatorComp.test.tsx
+++ __test__/CalculatorComp.test.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import Calculator from "../components/Calculator";
 
-
 it("renders all components", () => {
     const { getByText, getByTestId } = render(<Calculator />);
-    
+
     expect(getByTestId("display")).toBeTruthy();
     expect(getByTestId("1")).toBeTruthy();
     expect(getByTestId(".")).toBeTruthy();
     expect(getByTestId("+")).toBeTruthy();
     expect(getByTestId("=")).toBeTruthy();
     expect(getByTestId("AC")).toBeTruthy();
+});
+
+it("updates display when numbers are pressed", () => {
+    const { getByText, getByTestId } = render(<Calculator />);
+    fireEvent.press(getByTestId("1"));
+    fireEvent.press(getByTestId("2"));
+    expect(getByTestId("display").props.children).toBe("12");
 });
