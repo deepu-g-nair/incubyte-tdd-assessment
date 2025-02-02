16ba4a6236746a9a7fa18a4a0461dc1b7fd1b3af
Deepu G Nair <deepu.g.nair@sporthood.in>
2025-02-02 10:53:17 +0530

    ui testing render components pass

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,5 +1,16 @@
-import { Text } from "react-native";
+import { Button, View } from "react-native";
 
-export default function Calculator(){
-    return <Text>Hello world</Text>
+
+export default function Calculator() {
+    return (
+        <View>
+            <View testID="display" />
+            <Button testID="0" title="0" />
+            <Button testID="1" title="1" />
+            <Button testID="." title="." />
+            <Button testID="+" title="+" />
+            <Button testID="=" title="=" />
+            <Button testID="AC" title="AC" />
+        </View>
+    );
 }
